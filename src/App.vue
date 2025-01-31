c399150d23b533aa97e0fe508907b437ad35892f
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-31 10:49:56 -0500

    First Components and Props

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
   </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
 export default {
+  components: { LearningResource },
+  component: {
+    LearningResource,
+  },
   data() {
     return {
       storedResources: [
