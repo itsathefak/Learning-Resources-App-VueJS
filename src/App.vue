fdda73988165a6c849d54d9e6622b72a679cc680
Athef Ak <athefak@Athefs-MacBook-Pro.local>
2025-01-31 10:34:41 -0500

    Set up and First steps

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'This is the Official VueJS Documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
